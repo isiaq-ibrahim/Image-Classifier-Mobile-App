e980a753b38c490aa9a8c1a41cc1c169ddd6a67e
isiaq-ibrahim <boltechtechnologies99@gmail.com>
2024-07-07 16:10:31 +0300

    Key features Real-time Object Recognition Gallery Image Analysis Efficient TensorFlow Integration User-Friendly Interface Privacy and Security

---
+++ app/src/main/java/developer/boltech/technologies/imageclassifier/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package developer.boltech.technologies.imageclassifier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler = new Handler(Looper.getMainLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
